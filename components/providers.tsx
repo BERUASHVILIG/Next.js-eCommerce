5ccac53d03acfe2e3c7a30cdeb96c94913c37890
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-08-11 23:03:06 +0400

    render product

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Session } from "inspector"
 import { CartProvider } from "use-shopping-cart"
 
 import { Toaster } from "@/components/ui/toaster"
@@ -11,5 +12,20 @@ interface Props {
 }
 
 export function Providers({ children }: Props) {
-  return <>{children}</>
+  return (
+    <>
+      <CartProvider
+        currency="USD"
+        shouldPersist
+        cartMode="checkout-session"
+        stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster />
+          {children}
+          <TailwindIndicator />
+        </ThemeProvider>
+      </CartProvider>
+    </>
+  )
 }
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+    "sknyQqTKUIhnru9NKYxQECczD21WJdbW0rjjSgeimyJojuFt8dRxAPpLTRuWTHTHAMSWp9lAdzuVshHSHeSdzy5LPYGV04bqLWoAMFFCnrwYmgc07IJ9rzKzRXDqXKGg8RFqSLfEUmHlLvpssFT1x6UbXXiPmAjGiRPe4dMvFS8bbPXg9CLZ",
 })
